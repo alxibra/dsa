1fcde2c75b48fcd76d540a499160a00d8dbc8888
alxibra <alxibra@forstok.com>
2025-06-29 08:16:05 +0700

    add solution 2 for alternative

--- array/most-frequent-even/main.go
+++ array/most-frequent-even/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	elements := []int{0, 1, 2, 2, 4, 4, 4, 1}
-	fmt.Println("frequentIndex: ", solutionOne(elements))
+	fmt.Println("frequentIndex: ", solutionTwo(elements))
 }
 
 func solutionOne(elements []int) int {
@@ -25,6 +25,25 @@ func solutionOne(elements []int) int {
 		if count > frequent {
 			frequent = count
 			frequentIndex = element
+		}
+	}
+
+	return frequentIndex
+}
+
+func solutionTwo(elements []int) int {
+	frequentIndex := -1
+	frequent := 0
+	var counts [1000001]int
+
+	for _, element := range elements {
+		if element%2 == 0 {
+			counts[element]++
+			count := counts[element]
+			if count > frequent {
+				frequent = count
+				frequentIndex = element
+			}
 		}
 	}
 
