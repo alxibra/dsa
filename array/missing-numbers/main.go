f99e32e48d89cf86ecd8d23336231a2c0b1b63c9
alxibra <alxibra@forstok.com>
2025-06-29 19:26:50 +0700

    alternatvie solutionTwo

--- array/missing-numbers/main.go
+++ array/missing-numbers/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nums := []int{0, 1, 2, 50, 99, 100}
-	fmt.Println("missingNumbers: ", solutionOne(nums))
+	fmt.Println("missingNumbers: ", solutionTwo(nums))
 
 }
 
@@ -18,6 +18,22 @@ func solutionOne(nums []int) []int {
 	for index, count := range counts {
 		// if count = 0, it must not present in nums
 		if count == 0 {
+			missingNumbers = append(missingNumbers, index)
+		}
+	}
+	return missingNumbers
+}
+
+func solutionTwo(nums []int) []int {
+	var seen [101]bool
+	for _, n := range nums {
+		seen[n] = true
+	}
+
+	var missingNumbers []int
+	for index, found := range seen {
+		// if not found, it must not present in nums
+		if !found {
 			missingNumbers = append(missingNumbers, index)
 		}
 	}
