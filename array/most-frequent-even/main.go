13d6188720b9530ee7f177ac884b36c157cf8007
alxibra <alxibra@forstok.com>
2025-06-29 08:10:09 +0700

    solutionOne for most frequent even

---
+++ array/most-frequent-even/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := []int{0, 1, 2, 2, 4, 4, 4, 1}
+	fmt.Println("frequentIndex: ", solutionOne(elements))
+}
+
+func solutionOne(elements []int) int {
+	frequentIndex := -1
+	frequent := 0
+	var counts [1000001]int
+
+	for _, element := range elements {
+		if element%2 == 0 {
+			counts[element]++
+		}
+	}
+
+	for element, count := range counts {
+		if element%2 != 0 {
+			continue
+		}
+		if count > frequent {
+			frequent = count
+			frequentIndex = element
+		}
+	}
+
+	return frequentIndex
+}
