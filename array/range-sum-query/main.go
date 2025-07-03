c20298de1360ab7c1ea1322ab9ce3b41a01a8d4f
alxibra <alxibra@forstok.com>
2025-07-03 09:42:33 +0700

    add solutionTwo

--- array/range-sum-query/main.go
+++ array/range-sum-query/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	nums := []int{-2, 0, 3, -5, 2, -1}
 	fmt.Println("sum: ", solutionOne(2, 4, nums))
+	fmt.Println("sum: ", solutionTwo(2, 4, nums))
 }
 
+// NOTE: use this solution for better
 func solutionOne(left, right int, nums []int) int {
 	// Create a prefix sum array with an extra slot at the beginning
 	// prefix[0] is set to 0 to handle sum from index 0 gracefully
@@ -20,4 +24,17 @@ func solutionOne(left, right int, nums []int) int {
 
 	// Return the sum of elements from nums[left] to nums[right] inclusive
 	return prefix[right+1] - prefix[left]
+}
+
+func solutionTwo(left, right int, nums []int) int {
+	for i, num := range nums {
+		if i == 0 {
+			continue
+		}
+		nums[i] = nums[i-1] + num
+	}
+	if left == 0 {
+		return nums[right]
+	}
+	return nums[right] + nums[left-1]
 }
