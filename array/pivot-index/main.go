f767b7e053f88cc346e565f984d5908851a7f6d1
alxibra <alxibra@forstok.com>
2025-07-13 18:52:46 +0700

    add solutionTwo

--- array/pivot-index/main.go
+++ array/pivot-index/main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	nums := []int{1, 7, 3, 6, 5, 6}
 	fmt.Println("nums: ", nums)
-	fmt.Printf("pivotIndex: %d\n", solutionOne(nums))
+	// fmt.Printf("pivotIndex: %d\n", solutionOne(nums))
+	fmt.Printf("pivotIndex: %d\n", solutionTwo(nums))
 }
 
 func solutionOne(nums []int) int {
@@ -24,6 +25,34 @@ func solutionOne(nums []int) int {
 		rightSum := prefixMap[rightIndex] - prefixMap[index-1]
 		if rightSum == leftSum {
 			pivotIndex = index
+		}
+	}
+	return pivotIndex
+}
+
+func solutionTwo(nums []int) int {
+	pivotIndex := -1
+	prefixMap := make(map[int]int)
+
+	// Improvement: Reuse prefixMap variable to avoid extra space for prefix sum.
+	for index, num := range nums {
+		if index == 0 {
+			prefixMap[index] = num
+			continue
+		}
+		prefixMap[index] = prefixMap[index-1] + num
+	}
+
+	leftSum, rightSum := 0, 0
+
+	for key, value := range prefixMap {
+		if key == 0 {
+			continue
+		}
+		leftSum = value
+		rightSum = prefixMap[len(prefixMap)-1] - prefixMap[key-1]
+		if leftSum == rightSum {
+			return key
 		}
 	}
 	return pivotIndex
