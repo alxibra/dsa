c43b8d5f50ac0a49af18afdd0238d3de01ff0c0a
alxibra <alxibra@forstok.com>
2025-07-20 14:04:14 +0700

    add solution for count odd number

--- array/even-odd-count-in-range/main.go
+++ array/even-odd-count-in-range/main.go
@@ -3,8 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{0, 1, 1}
-	fmt.Printf("countEven: %d\n", solutionCountEvenNum(nums))
+	// nums := []int{0, 1, 1}
+	nums := []int{0, 1, 2}
+	// fmt.Printf("countEven: %d\n", solutionCountEvenNum(nums))
+	fmt.Printf("SolutionCountOddNum: %d\n", SolutionCountOddNum(nums))
 }
 
 func solutionCountEvenNum(nums []int) int {
@@ -13,9 +15,28 @@ func solutionCountEvenNum(nums []int) int {
 	for _, num := range nums {
 		prefixSum += num
 		mod = prefixSum % 2
-		count += hashTable[mod]
+		hashTable[mod]++
+	}
+	return count
+}
+
+func SolutionCountOddNum(nums []int) int {
+	var count, prefixSum, mod = 0, 0, 0
+
+	hashTable := make(map[int]int)
+
+	hashTable[0] = 1
+
+	for _, num := range nums {
+
+		prefixSum += num
+
+		mod = prefixSum % 2
+
+		count += hashTable[mod-1]
 
 		hashTable[mod]++
+
 	}
 	return count
 }
