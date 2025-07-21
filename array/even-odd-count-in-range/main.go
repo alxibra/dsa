01a50685e8e4b7c70f7b7c1be9cd7e6dff215767
alxibra <alxibra@forstok.com>
2025-07-21 19:40:14 +0700

    finishing even-odd-count-in-range

--- array/even-odd-count-in-range/main.go
+++ array/even-odd-count-in-range/main.go
@@ -3,19 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	// nums := []int{0, 1, 1}
-	nums := []int{0, 1, 2}
-	// fmt.Printf("countEven: %d\n", solutionCountEvenNum(nums))
-	fmt.Printf("SolutionCountOddNum: %d\n", SolutionCountOddNum(nums))
+	nums := []int{0, 1, 1}
+	// nums := []int{0, 1, 2}
+	fmt.Printf("countEven: %d\n", solutionCountEvenNum(nums))
+	// fmt.Printf("SolutionCountOddNum: %d\n", SolutionCountOddNum(nums))
 }
 
 func solutionCountEvenNum(nums []int) int {
+
 	var count, prefixSum, mod = 0, 0, 0
-	hashTable := map[int]int{0: 1}
+
+	hashTable := make(map[int]int)
+
+	hashTable[0] = 1
+
 	for _, num := range nums {
+
 		prefixSum += num
+
 		mod = prefixSum % 2
+
+		count += hashTable[mod]
+
 		hashTable[mod]++
+
 	}
 	return count
 }
