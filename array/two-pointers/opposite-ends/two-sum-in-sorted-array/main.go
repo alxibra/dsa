dda90f33f1ffa62252b724a656db144a752403ce
alxibra <alxibra@forstok.com>
2025-08-13 22:57:41 +0700

    two-sum-in-sorted-array

--- array/two-pointers/opposite-ends/two-sum-in-sorted-array/main.go
+++ array/two-pointers/opposite-ends/two-sum-in-sorted-array/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	fmt.Println("result: ", solutionOne(nums, target))
+}
+
+func solutionOne(nums []int, target int) []int {
+	var result []int
+	left, right, sum := 0, len(nums)-1, 0
+
+	if len(nums) < 2 {
+		return result
+	}
+
+	for left < right {
+		sum = nums[left] + nums[right]
+
+		if sum == target {
+			return []int{left, right}
+		}
+
+		if sum < target {
+			left++
+		}
+
+		if sum > target {
+			right--
+		}
+	}
+	return result
 }
