00bb5eea33155c2bb5ccdad2e553a502df80982c
alxibra <alxibra@forstok.com>
2025-08-17 10:12:53 +0700

    finishing reserve int

--- array/two-pointers/opposite-ends/reserve-array/main.go
+++ array/two-pointers/opposite-ends/reserve-array/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	nums := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println("nums: \t\t", nums)
+	fmt.Println("reverse: \t", solutionOne(nums))
+}
+
+func solutionOne(nums []int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		nums[left], nums[right] = nums[right], nums[left]
+		left++
+		right--
+	}
+	return nums
 }
