fd8a9dd5fe6ffacb482404204dab4fec4c727ba3
alxibra <alxibra@forstok.com>
2025-08-17 10:35:34 +0700

    finishing reverse array string

--- array/two-pointers/opposite-ends/reserve-array-string/main.go
+++ array/two-pointers/opposite-ends/reserve-array-string/main.go
@@ -3,5 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	str := "hello"
+
+	fmt.Println("solution: ", solutionOne(str))
+}
+
+func solutionOne(str string) string {
+	arr := []byte(str)
+
+	left, right := 0, len(arr)-1
+
+	for left < right {
+		arr[left], arr[right] = arr[right], arr[left]
+		left++
+		right--
+	}
+
+	return string(arr)
 }
