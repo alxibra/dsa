57d6d59bc755b34e615922c9d2a21716bf5a0bf4
alxibra <alxibra@forstok.com>
2025-08-26 10:35:59 +0700

    finishing trapped water problem

--- array/two-pointers/opposite-ends/trapping-rain-water/main.go
+++ array/two-pointers/opposite-ends/trapping-rain-water/main.go
@@ -4,26 +4,59 @@ import "fmt"
 
 func main() {
 	height := []int{4, 2, 0, 3, 2, 5}
+	// left, right := 0, len(height)-1
+	// leftMax, rightMax := 0, 0
+	// trap := 0
+	// for left < right {
+	// 	if height[left] < height[right] {
+	// 		if height[left] >= leftMax {
+	// 			leftMax = height[left]
+	// 		} else {
+	// 			trap += leftMax - height[left]
+	// 		}
+	// 		left++
+	// 	} else {
+	// 		if height[right] > rightMax {
+	// 			rightMax = height[right]
+	// 		} else {
+	// 			trap += rightMax - height[right]
+	// 		}
+	// 		right--
+	// 	}
+	// 	fmt.Println("")
+	// }
+
+	fmt.Println("trap: ", solutionOne(height))
+}
+
+func solutionOne(height []int) int {
+	var trap int
 	left, right := 0, len(height)-1
 	leftMax, rightMax := 0, 0
-	trap := 0
 	for left < right {
-		if height[left] < height[right] {
-			if height[left] >= leftMax {
-				leftMax = height[left]
+		// find the heigh of left and right
+		leftHeight, rightHeight := height[left], height[right]
+		// If leftHeight < rightHeight, we process the left side first
+		// because the amount of water trapped is limited by the shorter bar.
+		if leftHeight < rightHeight {
+			if leftHeight > leftMax {
+				// No water is trapped when leftHeight exceeds leftMax; update leftMax
+				leftMax = leftHeight
 			} else {
-				trap += leftMax - height[left]
+				// Water trapped is the difference between leftMax and leftHeight
+				trap += leftMax - leftHeight
 			}
 			left++
 		} else {
-			if height[right] > rightMax {
-				rightMax = height[right]
+			if rightHeight > rightMax {
+				// No water is trapped when rightHeight exceeds rightMax, update rightMax
+				rightMax = rightHeight
 			} else {
-				trap += rightMax - height[right]
+				// Water trapped is the difference between rightMax and leftHeight
+				trap += rightMax - rightHeight
 			}
 			right--
 		}
 	}
-
-	fmt.Println("trap: ", trap)
+	return trap
 }
