886d3991a5a2ab8f9093b960e396e031ed4d9644
alxibra <alxibra@forstok.com>
2025-08-26 10:37:10 +0700

    remove comment

--- array/two-pointers/opposite-ends/trapping-rain-water/main.go
+++ array/two-pointers/opposite-ends/trapping-rain-water/main.go
@@ -4,28 +4,6 @@ import "fmt"
 
 func main() {
 	height := []int{4, 2, 0, 3, 2, 5}
-	// left, right := 0, len(height)-1
-	// leftMax, rightMax := 0, 0
-	// trap := 0
-	// for left < right {
-	// 	if height[left] < height[right] {
-	// 		if height[left] >= leftMax {
-	// 			leftMax = height[left]
-	// 		} else {
-	// 			trap += leftMax - height[left]
-	// 		}
-	// 		left++
-	// 	} else {
-	// 		if height[right] > rightMax {
-	// 			rightMax = height[right]
-	// 		} else {
-	// 			trap += rightMax - height[right]
-	// 		}
-	// 		right--
-	// 	}
-	// 	fmt.Println("")
-	// }
-
 	fmt.Println("trap: ", solutionOne(height))
 }
 
