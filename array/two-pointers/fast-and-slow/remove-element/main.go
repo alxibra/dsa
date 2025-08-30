eeb572b779228d6601c5ffb9e7bdfb48b52aab18
alxibra <alxibra@forstok.com>
2025-08-30 20:34:44 +0700

    move to function

--- array/two-pointers/fast-and-slow/remove-element/main.go
+++ array/two-pointers/fast-and-slow/remove-element/main.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+// Input:  nums = [3,2,2,3], val = 3
+// Output: 2
+// Explanation: Your function should return length = 2,
+// and the first two elements of nums should be 2 and 2.
+
 func main() {
-	fmt.Println("hellow world")
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	val := 2
+	fmt.Println("Output: ", solutionOne(val, nums))
+}
+
+func solutionOne(val int, nums []int) int {
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+		fast++
+	}
+	return slow
 }
